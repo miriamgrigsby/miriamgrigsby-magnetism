2210d6d0930fc1a1ebaa2988ba48a35b82fd7464
Miriam Grigsby <miriamgrigsby@gmail.com>
2020-04-12 12:55:03 -0600

    about-me button

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,6 +7,12 @@ const AboutMe = () => {
             <LinkButtons />
             <div className="about-me">
                 <div className="top-about-me">
+                    <h1 id="aboutme-title"> Miriam Grigsby</h1>
+                    <h1 id="aboutme-title2"> Full-stack developer</h1>
+                    <div id="about-me-buttonDiv">
+                        <button id="about-me-button">About Me</button>
+                        <img></img>
+                    </div>
                 </div>
                 <div className="middle-about-me">
                     
